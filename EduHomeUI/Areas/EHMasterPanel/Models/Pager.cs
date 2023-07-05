2b5fb316b861e33f9fea72aa40049d51845ba073
Rahil Habibli <rahilah@code.edu.az>
2023-07-06 01:11:36 +0400

    Pagination start

--- EduHomeUI/Areas/EHMasterPanel/Models/Pager.cs
+++ EduHomeUI/Areas/EHMasterPanel/Models/Pager.cs
@@ -2,44 +2,43 @@
 {
 	public class Pager
 	{
-        public int TotalItems { get; private set; }
-        public int CurrentPage { get; private set; }
-        public int PageSize { get; private set; }
-        public int TotalPages { get; private set; }
-        public int StartPage { get; private set; }
-        public int EndPage { get; private set; }
+		public int TotalItems { get; private set; }
+		public int CurrentPage { get; private set; }
+		public int PageSize { get; private set; }
+		public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / PageSize);
+		public int StartPage { get; private set; }
+		public int EndPage { get; private set; }
 
-        public Pager()
-        {
-            
-        }
-        public Pager(int totalItems, int page, int pageSize = 10)
-        {
-            int totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
-            int currentPage = page;
+		public Pager(int totalItems, int page, int pageSize = 10)
+		{
+			TotalItems = totalItems;
+			CurrentPage = page;
+			PageSize = pageSize;
 
-            int startPage = currentPage - 5;
-            int endPage = currentPage + 4;
+			CalculateStartAndEndPages();
+		}
 
-            if (startPage <= 0)
-            {
-                endPage = endPage - (startPage - 1);
-                startPage = 1;
-            }
-            if (endPage > totalPages)
-            {
-                endPage = totalPages;
-                if (endPage > 10)
-                {
-                    startPage = endPage - 9;
-                }
-            }
-            TotalItems = totalItems;
-            CurrentPage = currentPage;
-            StartPage = startPage;
-            EndPage = endPage;
-            TotalPages = totalPages;
-            PageSize  = pageSize;
-        }
-    }
+		private void CalculateStartAndEndPages()
+		{
+			int startPage = CurrentPage - 5;
+			int endPage = CurrentPage + 4;
+
+			if (startPage <= 0)
+			{
+				endPage = endPage - (startPage - 1);
+				startPage = 1;
+			}
+			if (endPage > TotalPages)
+			{
+				endPage = TotalPages;
+				if (endPage > 10)
+				{
+					startPage = endPage - 9;
+				}
+			}
+
+			StartPage = startPage;
+			EndPage = endPage;
+		}
+	}
 }
--- EduHomeUI/Areas/EHMasterPanel/ViewModels/CourseCategoryViewModel.cs
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/CourseCategoryViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace EduHomeUI.Areas.EHMasterPanel.ViewModels
+﻿using EduHome.Core.Entities;
+using EduHomeUI.Areas.EHMasterPanel.Models;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels
 {
 	public class CourseCategoryViewModel
 	{
 		public string Category { get; set; }
 		public DateTime DateModified { get; set; }
+
 	}
 }
