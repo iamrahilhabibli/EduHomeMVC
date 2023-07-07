9bf28cc2a45b9929206a95b437b85816b7838fc9
Rahil Habibli <rahilah@code.edu.az>
2023-07-07 13:26:36 +0400

    Skill Add Service done

---
+++ EduHomeUI/Areas/EHMasterPanel/Controllers/SkillController.cs
@@ -0,0 +1,40 @@
+﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.AssesmentViewModels;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.SkillViewModels;
+using EduHomeUI.Services.Concretes;
+using EduHomeUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHomeUI.Areas.EHMasterPanel.Controllers
+{
+    [Area("EHMasterPanel")]
+    public class SkillController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ISkillService _skillService;
+
+        public SkillController(AppDbContext context,ISkillService skillService)
+        {
+            _context = context;
+            _skillService = skillService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SkillViewModel skillVm)
+        {
+            if (!ModelState.IsValid) return NotFound();
+            if (!await _skillService.CreateSkillAsync(skillVm)) return BadRequest();
+            TempData["Success"] = "Skill Created Successfully!";
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+    }
+}
