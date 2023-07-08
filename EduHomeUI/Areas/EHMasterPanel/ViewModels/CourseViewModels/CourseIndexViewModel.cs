c0428a587984c97b81f06c725f930eb860eb945a
Rahil Habibli <rahilah@code.edu.az>
2023-07-08 09:02:56 +0400

    Course Index View - View Model Adding

---
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/CourseViewModels/CourseIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using EduHome.Core.Entities;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.CourseViewModels
+{
+	public class CourseIndexViewModel
+	{
+		public IEnumerable<Course> courses { get; set; }
+	}
+}
