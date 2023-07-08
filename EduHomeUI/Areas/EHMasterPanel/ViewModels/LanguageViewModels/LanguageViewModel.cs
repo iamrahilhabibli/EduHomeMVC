7b083d99554fb336ee37f6d3622fb44d553ab4c3
Rahil Habibli <rahilah@code.edu.az>
2023-07-08 20:27:40 +0400

    language and skill validations added

--- EduHomeUI/Areas/EHMasterPanel/ViewModels/LanguageViewModels/LanguageViewModel.cs
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/LanguageViewModels/LanguageViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.LanguageViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.LanguageViewModels
 {
-	public class LanguageViewModel
-	{
-		public string? LanguageOption { get; set; }
-	}
+    public class LanguageViewModel
+    {
+        [Required(ErrorMessage = "Language option is required")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Language option can only contain letters")]
+        [MaxLength(20, ErrorMessage = "Language option cannot exceed 20 characters")]
+        public string? LanguageOption { get; set; }
+    }
 }
--- EduHomeUI/Areas/EHMasterPanel/ViewModels/SkillViewModels/SkillViewModel.cs
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/SkillViewModels/SkillViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.SkillViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.SkillViewModels
 {
     public class SkillViewModel
     {
+        [Required(ErrorMessage = "Skill is required")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Skill can only contain letters")]
+        [MaxLength(20, ErrorMessage = "Skill cannot exceed 20 characters")]
         public string? Skill { get; set; }
     }
 }
