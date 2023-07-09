ac497b570f900b0e59a03a4820c903487f72a381
Rahil Habibli <rahilah@code.edu.az>
2023-07-09 16:23:29 +0400

    Speaker create service added

--- EduHomeUI/Services/Concretes/SpeakerService.cs
+++ EduHomeUI/Services/Concretes/SpeakerService.cs
@@ -1,5 +1,7 @@
 ﻿using EduHome.Core.Entities;
 using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.LanguageViewModels;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.SpeakerViewModels;
 using EduHomeUI.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +14,25 @@ namespace EduHomeUI.Services.Concretes
         {
             _context = context;
         }
+
+        public async Task<bool> CreateSpeakerAsync(SpeakerCreateViewModel speakerVm)
+        {
+            if (speakerVm is null) return false;
+            Speaker newSpeaker = new()
+            {
+                Name = speakerVm.Name,
+                Surname = speakerVm.Surname,
+                ImageName = speakerVm.ImageName,
+                ImagePath = speakerVm.ImagePath,
+                Position = speakerVm.Position,
+                CompanyName = speakerVm.CompanyName,
+                IsDeleted = false
+            };
+            _context.Speakers.Add(newSpeaker);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<List<Speaker>> GetAllSpeakers()
         {
             return await _context.Speakers.ToListAsync();
--- EduHomeUI/Services/Interfaces/ISpeakerService.cs
+++ EduHomeUI/Services/Interfaces/ISpeakerService.cs
@@ -1,9 +1,12 @@
 ﻿using EduHome.Core.Entities;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.LanguageViewModels;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.SpeakerViewModels;
 
 namespace EduHomeUI.Services.Interfaces
 {
     public interface ISpeakerService
     {
         Task<List<Speaker>> GetAllSpeakers();
+        Task<bool> CreateSpeakerAsync(SpeakerCreateViewModel speakerVm);
     }
 }
