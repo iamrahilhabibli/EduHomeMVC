6510cb5d8cc69fe480f104830f167a056379f3ab
Rahil Habibli <rahilah@code.edu.az>
2023-07-09 11:30:54 +0400

    Teacher create mapper added

---
+++ EduHomeUI/Mappers/TeacherCreateProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EduHome.Core.Entities;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.TeacherViewModels;
+
+namespace EduHomeUI.Mappers
+{
+    public class TeacherCreateProfile:Profile
+    {
+        public TeacherCreateProfile()
+        {
+            CreateMap<TeacherCreateViewModel,Teacher>().ReverseMap();
+            CreateMap<TeacherCreateViewModel, TeacherDetails>().ReverseMap();
+        }
+    }
+}
--- EduHomeUI/Services/Concretes/TeacherService.cs
+++ EduHomeUI/Services/Concretes/TeacherService.cs
@@ -25,32 +25,13 @@ namespace EduHomeUI.Services.Concretes
         {
             if (teacherVm is null) return false;
 
-            Teacher teacher = new Teacher()
-            {
-                Name = teacherVm.Name,
-                Surname = teacherVm.Surname,
-                ImageName = teacherVm.ImageName,
-                ImagePath = teacherVm.ImagePath,
-                Position = teacherVm.Position,
-            };
-            teacher.TeacherDetails = new TeacherDetails
-            {
-                Description = teacherVm.Description,
-                Email = teacherVm.Email,
-                PhoneNumber = teacherVm.PhoneNumber,
-                SkypeAddress = teacherVm.SkypeAddress,
-                LanguageSkills = teacherVm.LanguageSkills,
-                TeamLeaderSkills = teacherVm.TeamLeaderSkills,
-                DevelopmentSkills = teacherVm.DevelopmentSkills,
-                Design = teacherVm.Design,
-                Innovation = teacherVm.Innovation,
-                Communication = teacherVm.Communication
-            };
-            await _context.Teachers.AddAsync(teacher);  
-           await _context.SaveChangesAsync();
+            Teacher teacher = _mapper.Map<Teacher>(teacherVm);
+            teacher.TeacherDetails = _mapper.Map<TeacherDetails>(teacherVm);
+            await _context.Teachers.AddAsync(teacher);
+            await _context.SaveChangesAsync();
             return true;
         }
-  
+
         public async Task<List<Teacher>> GetAllTeachersAsync()
         {
             return await _context.Teachers.ToListAsync();
