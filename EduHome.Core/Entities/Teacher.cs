34f5acbf81342698404fcb7b1197908a5913b828
Rahil Habibli <rahilah@code.edu.az>
2023-07-09 09:15:29 +0400

    Teacher Entity Start

---
+++ EduHome.Core/Entities/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using EduHome.Core.Entities.Common;
+
+namespace EduHome.Core.Entities
+{
+    public class Teacher:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImagePath { get; set; }
+        public string ImageName { get; set; }
+        public string Position { get; set; }
+
+    }
+}
