2a2086b0ad58c119d66613c491975e4597d8a862
Rahil Habibli <rahilah@code.edu.az>
2023-07-10 01:31:35 +0400

    AuthControllerFixed

---
+++ EduHomeUI/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHomeUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
