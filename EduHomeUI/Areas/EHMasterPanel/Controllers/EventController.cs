ef9b82365737a32c9b9dbbeb860169074eef4eb3
Rahil Habibli <rahilah@code.edu.az>
2023-07-09 15:19:48 +0400

    events index done

--- EduHomeUI/Areas/EHMasterPanel/Controllers/EventController.cs
+++ EduHomeUI/Areas/EHMasterPanel/Controllers/EventController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.CourseViewModels;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.EventViewModels;
+using EduHomeUI.Services.Concretes;
+using EduHomeUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IEventService _eventService;
+        public EventController(AppDbContext context, IMapper mapper, IEventService eventService)
         {
-            return View();
+            _context = context;
+            _mapper = mapper;   
+            _eventService = eventService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var eventsList = await _eventService.GetAllEventsAsync();
+
+            var viewModel = new EventIndexViewModel
+            {
+                Events = eventsList
+            };
+
+            return View(viewModel);
         }
     }
 }
