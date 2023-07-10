2f6f56b14d370c6e593093cb8ae755c257b217a8
Rahil Habibli <rahilah@code.edu.az>
2023-07-10 10:46:50 +0400

    Password validation summary added

--- EduHomeUI/Controllers/AuthController.cs
+++ EduHomeUI/Controllers/AuthController.cs
@@ -1,20 +1,42 @@
-﻿using EduHomeUI.ViewModels.UsersViewModel;
+﻿using EduHome.Core.Entities;
+using EduHomeUI.ViewModels.UsersViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduHomeUI.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AuthController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Register()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(UserRegisterViewModel userRegVm)
+        public async Task<IActionResult> Register(UserRegisterViewModel userRegVm)
         {
             if (!ModelState.IsValid) return View(userRegVm);
-            return View();
+            AppUser user = new()
+            {
+                FirstName = userRegVm.FirstName,
+                LastName = userRegVm.LastName,
+                Email = userRegVm.EmailAddress
+            };
+            IdentityResult result = await _userManager.AddPasswordAsync(user, userRegVm.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(userRegVm); 
+            }
+            return Ok();
         }
     }
 }
