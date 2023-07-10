1b54bf0506af1e433e28ed945e44e5506d091f8c
Rahil Habibli <rahilah@code.edu.az>
2023-07-11 00:49:49 +0400

    googleReCaptcha site key secret key added

---
+++ EduHomeUI/ViewModels/CaptchaViewModel/CaptchaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHomeUI.ViewModels.CaptchaViewModel
+{
+	public class CaptchaViewModel
+	{
+		[Required]
+        public string Captcha { get; set; }
+    }
+}
