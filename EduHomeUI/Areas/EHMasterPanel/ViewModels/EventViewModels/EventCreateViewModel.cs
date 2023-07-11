b53e5c79094e79f0cde2dd21102df09e690df887
Rahil Habibli <rahilah@code.edu.az>
2023-07-12 00:21:11 +0400

    Event view fv done

--- EduHomeUI/Areas/EHMasterPanel/ViewModels/EventViewModels/EventCreateViewModel.cs
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/EventViewModels/EventCreateViewModel.cs
@@ -7,25 +7,20 @@ namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.EventViewModels
 {
     public class EventCreateViewModel
     {
-        [Required(ErrorMessage = "Title is required.")]
+
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "ImagePath is required.")]
         public string ImagePath { get; set; }
 
-        [Required(ErrorMessage = "ImageName is required.")]
+    
         public string ImageName { get; set; }
 
-        [Required(ErrorMessage = "Venue is required.")]
         public string Venue { get; set; }
 
-        [Required(ErrorMessage = "StartTime is required.")]
         public DateTime StartTime { get; set; }
 
-        [Required(ErrorMessage = "EndTime is required.")]
         public DateTime EndTime { get; set; }
 
-        [Required(ErrorMessage = "Date is required.")]
         [DataType(DataType.Date)]
         public DateTime Date { get; set; } = DateTime.Now.Date;
 
---
+++ EduHomeUI/Validators/EventViewModelValidator.cs
@@ -0,0 +1,36 @@
+﻿using EduHome.Core.Entities;
+using FluentValidation;
+using System;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.EventViewModels
+{
+    public class EventViewModelValidator : AbstractValidator<EventCreateViewModel>
+    {
+        public EventViewModelValidator()
+        {
+            RuleFor(vm => vm.Title)
+                .NotEmpty().WithMessage("Title is required.");
+
+            RuleFor(vm => vm.ImagePath)
+                .NotEmpty().WithMessage("ImagePath is required.");
+
+            RuleFor(vm => vm.ImageName)
+                .NotEmpty().WithMessage("ImageName is required.");
+
+            RuleFor(vm => vm.Venue)
+                .NotEmpty().WithMessage("Venue is required.");
+
+            RuleFor(vm => vm.StartTime)
+                .NotEmpty().WithMessage("StartTime is required.");
+
+            RuleFor(vm => vm.EndTime)
+                .NotEmpty().WithMessage("EndTime is required.")
+                .GreaterThanOrEqualTo(vm => vm.StartTime).WithMessage("EndTime must be greater than or equal to StartTime.");
+
+            RuleFor(vm => vm.Date)
+                .NotEmpty().WithMessage("Date is required.")
+                .GreaterThanOrEqualTo(DateTime.Now.Date).WithMessage("Date cannot be in the past.");
+
+        }
+    }
+}
