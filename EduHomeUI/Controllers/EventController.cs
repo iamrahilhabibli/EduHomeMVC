b4507966b28edbf279121c5a6ea32d552d75dbb4
Rahil Habibli <rahilah@code.edu.az>
2023-07-13 00:43:01 +0400

    event - event details done

--- EduHomeUI/Controllers/EventController.cs
+++ EduHomeUI/Controllers/EventController.cs
@@ -1,4 +1,6 @@
-﻿using EduHome.DataAccess.Contexts;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.EventViewModels;
 using EduHomeUI.ViewModels.EventViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,11 +16,31 @@ namespace EduHomeUI.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            EventIndexViewModel viewModel = new EventIndexViewModel()
+            ViewModels.EventViewModels.EventIndexViewModel viewModel = new()
             {
                 Events = await _context.Events.ToListAsync()
             };
             return View(viewModel);
         }
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var viewModel = new ViewModels.EventViewModels.EventIndexViewModel();
+
+            var @event = await _context.Events
+                .Include(e => e.EventSpeakers)
+                .ThenInclude(es => es.Speaker)
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            viewModel.Events = new List<Event> { @event };
+            viewModel.EventSpeakers = @event.EventSpeakers;
+
+            return View(viewModel);
+        }
+
     }
 }
--- EduHomeUI/ViewModels/EventViewModels/EventIndexViewModel.cs
+++ EduHomeUI/ViewModels/EventViewModels/EventIndexViewModel.cs
@@ -5,5 +5,8 @@ namespace EduHomeUI.ViewModels.EventViewModels
     public class EventIndexViewModel
     {
         public IEnumerable<Event> Events { get; set; }
+        public IEnumerable<EventDetails> Details { get; set; }
+        public IEnumerable<Speaker> Speakers { get; set; }
+        public IEnumerable<EventSpeaker> EventSpeakers { get; set;}
     }
 }
