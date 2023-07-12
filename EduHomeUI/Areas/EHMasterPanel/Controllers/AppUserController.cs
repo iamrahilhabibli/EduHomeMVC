227dc964d24b86d0bee7d6b6cf9eddac5b3207a3
Rahil Habibli <rahilah@code.edu.az>
2023-07-12 14:22:09 +0400

    User table added to dashboard

--- EduHomeUI/Areas/EHMasterPanel/Controllers/AppUserController.cs
+++ EduHomeUI/Areas/EHMasterPanel/Controllers/AppUserController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.AppUserViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers
 {
+    [Area("EHMasterPanel")]
     public class AppUserController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public AppUserController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            AppUserIndexViewModel model = new AppUserIndexViewModel()
+            {
+                appUsers = await _context.Users.ToListAsync()
+            };
+
+            return View(model);
+        }
+
     }
 }
