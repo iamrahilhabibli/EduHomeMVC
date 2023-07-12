38f447c39006288da6ba1b2bd02b1625542f4c7b
Rahil Habibli <rahilah@code.edu.az>
2023-07-12 18:21:08 +0400

    AdminDashBoard welcome name added

--- EduHomeUI/Areas/EHMasterPanel/Controllers/DashboardController.cs
+++ EduHomeUI/Areas/EHMasterPanel/Controllers/DashboardController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers
 {
     [Area("EHMasterPanel")]
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+                _context = context;
+        }
         public IActionResult Index()
         {
+            string firstName = string.Empty;
+            if (User.Identity.IsAuthenticated)
+            {
+                string username = User.Identity.Name;
+                AppUser user = _context.Users.FirstOrDefault(u => u.UserName == username);
+                if (user != null)
+                {
+                    firstName = user.FirstName;
+                }
+            }
+            ViewBag.FirstName = firstName;
             return View();
         }
+
     }
 }
