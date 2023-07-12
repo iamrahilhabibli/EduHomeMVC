db988e09b6aca77f711672440000ec968694b587
Rahil Habibli <rahilah@code.edu.az>
2023-07-12 23:44:43 +0400

    Event view done

--- EduHomeUI/Controllers/EventController.cs
+++ EduHomeUI/Controllers/EventController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels.EventViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public EventController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            EventIndexViewModel viewModel = new EventIndexViewModel()
+            {
+                Events = await _context.Events.ToListAsync()
+            };
+            return View(viewModel);
         }
     }
 }
