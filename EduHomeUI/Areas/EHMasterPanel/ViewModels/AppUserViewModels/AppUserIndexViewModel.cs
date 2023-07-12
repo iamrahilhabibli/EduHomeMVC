f43cf70fd259953a02b4fce67b9b7542df271a84
Rahil Habibli <rahilah@code.edu.az>
2023-07-12 14:14:36 +0400

    Users index view added

---
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/AppUserViewModels/AppUserIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using EduHome.Core.Entities;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.AppUserViewModels
+{
+    public class AppUserIndexViewModel
+    {
+        public IEnumerable<AppUser> appUsers { get; set; }
+    }
+}
