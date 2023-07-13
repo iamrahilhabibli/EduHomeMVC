7b2f3646b8cd92fd74c953bd46ba119597264b58
Rahil Habibli <rahilah@code.edu.az>
2023-07-13 10:33:57 +0400

    blog page view done

---
+++ EduHomeUI/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels.BlogViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            BlogIndexViewModel viewModel = new BlogIndexViewModel()
+            {
+                Blogs = await _context.Blogs.ToListAsync()
+            };
+            return View(viewModel);
+        }
+        public async Task<IActionResult> Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ EduHomeUI/ViewModels/BlogViewModels/BlogIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EduHome.Core.Entities;
+
+namespace EduHomeUI.ViewModels.BlogViewModels
+{
+    public class BlogIndexViewModel
+    {
+        public IEnumerable<Blog> Blogs { get; set; }
+        public IEnumerable<BlogDetails> BlogDetails { get; set; }    
+    }
+}
