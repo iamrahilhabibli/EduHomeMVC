99b023973348fbd85b58d8bba2bbc620b75dc36f
Rahil Habibli <rahilah@code.edu.az>
2023-07-13 09:48:37 +0400

    teachers view added

---
+++ EduHomeUI/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.Areas.EHMasterPanel.ViewModels.TeacherViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeUI.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeacherController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            TeacherIndexViewModel model = new TeacherIndexViewModel()
+            {
+                Teachers = await _context.Teachers.ToListAsync()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ EduHomeUI/ViewModels/TeacherViewModels/TeacherIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+using EduHome.Core.Entities;
+
+namespace EduHomeUI.ViewModels.TeacherViewModels
+{
+    public class TeacherIndexViewModel
+    {
+        public IEnumerable<Teacher> Teachers { get; set; }
+    }
+}
