f50ee3aa6f6d90da82e1bc00b239fa67b3db5f45
Rahil Habibli <rahilah@code.edu.az>
2023-07-13 19:09:57 +0400

    setting entity start

---
+++ EduHome.Core/Entities/Setting.cs
@@ -0,0 +1,11 @@
+﻿using EduHome.Core.Entities.Common;
+
+namespace EduHome.Core.Entities
+{
+    public class Setting:BaseEntity
+    {
+        public string Key { get; set; } = null!;
+        public string Value { get; set; } = null!;
+        public override bool IsDeleted { get; set; }
+    }
+}
