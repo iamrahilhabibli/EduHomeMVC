fb048d3256daf745c09583dac7ac5e460b4d091c
Rahil Habibli <rahilah@code.edu.az>
2023-07-14 00:04:30 +0400

    footer viewcomponentdone

---
+++ EduHomeUI/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeUI.ViewComponents
+{
+	public class FooterViewComponent:ViewComponent
+	{
+		private readonly AppDbContext _context;
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Dictionary<string, string> setting = await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+            return View(setting);
+        }
+    }
+}
