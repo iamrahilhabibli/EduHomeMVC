f79b2815f8dd7c00492dada1dfa7b7d4a7e1dd4c
Rahil Habibli <rahilah@code.edu.az>
2023-07-17 00:10:41 +0400

    final v

---
+++ EduHomeUI/Areas/EHMasterPanel/ViewModels/EnrolledStudentsViewModels/EnrolledStudentIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EduHome.Core.Entities;
+using System.Collections.Generic;
+
+namespace EduHomeUI.Areas.EHMasterPanel.ViewModels.EnrolledStudentsViewModels
+{
+    public class EnrolledStudentIndexViewModel
+    {
+        public IEnumerable<ConfirmedStudents> ConfirmedStudents { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
