81593dbdec1f59f037008b9d1b9b45349c4e14ef
Rahil Habibli <rahilah@code.edu.az>
2023-07-16 16:45:48 +0400

    unathenticated user redirect buynow

--- EduHomeUI/Controllers/BuyNowController.cs
+++ EduHomeUI/Controllers/BuyNowController.cs
@@ -1,6 +1,8 @@
-﻿using EduHome.DataAccess.Contexts;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
 using EduHomeUI.Areas.EHMasterPanel.ViewModels.CourseViewModels;
 using EduHomeUI.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Web.Mvc;
 
@@ -11,13 +13,19 @@ namespace EduHomeUI.Controllers
     {
         private readonly AppDbContext _context;
         private readonly ICourseService _courseService;
-        public BuyNowController(AppDbContext context, ICourseService courseService)
+        private readonly UserManager<AppUser> _userManager;
+        public BuyNowController(AppDbContext context, ICourseService courseService,UserManager<AppUser> userManager)
         {
             _context = context;
             _courseService = courseService;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
             var coursesList = await _courseService.GetAllCourseAsync();
 
             var viewModel = new CourseIndexViewModel
